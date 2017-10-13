b4fe98ff2392e00defc99028ebe2f0d3c96409d3
Patrick9167 <leep2@tcd.ie>
2017-10-13 16:40:00 +0100

    Error test for a node not in the tree. Should fail.

--- BSTtest.hs
+++ BSTtest.hs
@@ -12,3 +12,7 @@ describe "LCA" $ do
 
   it "returns the lowest common ancestor of nodes 5 and 7; should be 7" $
     myLCA 5 7 `shouldBe` 7
+
+
+  it "returns the lowest common ancestor of nodes 2 and 12; should fail as 12 is not a node in the tree" $
+    myLCA 2 12 `shouldBe` error "Node not in tree"
