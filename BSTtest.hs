909d841e8a36ddc636326ec4b785b03368d5ddd1
Patrick9167 <leep2@tcd.ie>
2017-10-13 16:38:53 +0100

    Set up test file. Added 2 tests for lca.

--- BSTtest.hs
+++ BSTtest.hs
@@ -1,1 +1,14 @@
+module BSTtest where
 
+import Test.Hspec
+
+import BSTmain
+main :: IO ()
+main = hspec $ do
+
+describe "LCA" $ do
+  it "returns the lowest common ancestor of nodes 2 and 7; should be 4" $
+    myLCA 2 7 `shouldBe` 4
+
+  it "returns the lowest common ancestor of nodes 5 and 7; should be 7" $
+    myLCA 5 7 `shouldBe` 7
