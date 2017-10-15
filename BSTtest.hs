bc58f1fa255648441920afcb2001613eb8f6b365
Patrick9167 <leep2@tcd.ie>
2017-10-15 15:14:58 +0100

    Added more tests.

--- BSTtest.hs
+++ BSTtest.hs
@@ -5,14 +5,41 @@ import Test.Hspec
 import BSTmain
 main :: IO ()
 main = hspec $ do
+--describe "LCA" $ do
+  --it "Returns the lowest common ancestor of nodes within a binary search tree"
+describe "test" $ do
+  it "returns the lowest common ancestor of nodes 4 and 9; should be 8" $
+    mylca 4 9 `shouldBe` 8
 
-describe "LCA" $ do
-  it "returns the lowest common ancestor of nodes 2 and 7; should be 4" $
-    myLCA 2 7 `shouldBe` 4
+  it "returns the lowest common ancestor of nodes 9 and 4; should be 8" $
+    mylca 9 4 `shouldBe` 8
 
-  it "returns the lowest common ancestor of nodes 5 and 7; should be 7" $
-    myLCA 5 7 `shouldBe` 7
+  it "returns the lowest common ancestor of nodes 4 and 4; should be 4" $
+    mylca 4 4 `shouldBe` 4
 
+  it "returns the lowest common ancestor of nodes 10 and 2; should be 8" $
+    mylca 10 2 `shouldBe` 8
 
-  it "returns the lowest common ancestor of nodes 2 and 12; should fail as 12 is not a node in the tree" $
-    myLCA 2 12 `shouldBe` error "Node not in tree"
+  it "returns the lowest common ancestor of nodes 7 and 12; should be 8" $
+    mylca 7 12 `shouldBe` 8
+
+  it "returns the lowest common ancestor of nodes 9 and 11; should be 9" $
+    mylca 9 11 `shouldBe` 9
+
+  it "returns the lowest common ancestor of nodes 11 and 9; should be 9" $
+    mylca 11 9 `shouldBe` 9
+
+  it "returns the lowest common ancestor of nodes 2 and 4; should be 4" $
+    mylca 2 4 `shouldBe` 4
+
+  it "returns the lowest common ancestor of nodes 4 and 2; should be 4" $
+    mylca 4 2 `shouldBe` 4
+
+  it "ERROR test: two nodes not in tree" $
+    mylca 6 13 `shouldBe` error "Node not in tree"
+
+  it "ERROR test: first node not in tree" $
+    mylca 3 7 `shouldBe` error "Node not in tree"
+
+  it "ERROR test: second node not tree" $
+    mylca 7 3 `shouldBe` error "Node not in tree"
